60605c5c4f930f4ca4164f4ef96be145037d1dbc
Vens <v.kolev@alfatrex.com>
2022-01-13 10:02:07 +0100

    vens fix

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 
 // ## Aufgabenstellung - Teil 1
 // Schreibe ein Array mit mindestens fünf Namen und folgenden Personen: 'Pippi Longstocking', 'Bartholomew Roberts' und 'Hans Holbein the Younger'.
-const namen = ['Pippi Longstocking', 'Bartholomew Roberts', 'Hans Holbein the Younger'];
-const aufgabenstellung = namen.filter(long => long.length);
+const namen = ['Pippi Longstocking', 'Bartholomew Roberts', 'Hans Holbein the Younger', 'Bartholomew'];
+
 // ## Aufgabenstellung - Teil 2
 // Wende nun die filter()-Methode auf das Array an, um nur Namen zu filtern, deren Länge kleiner oder gleich 15 ist.
-function filter(long) { 
-    return long.length <= 15;
-
-}
+const aufgabenstellung = namen.filter((item) => {
+return item.length <= 15;
+});
 
 // ## Aufgabenstellung - Teil 3
 // Versuche nun den Gegensatz - Nur Namen länger als 15 Zeichen.
-
+const aufgabenstellung1 = namen.filter((item) => {
+  return item.length >= 15;
+});
 console.log(aufgabenstellung);
+console.log(aufgabenstellung1);
