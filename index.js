4b279ab6445e690411f9323a57ec00c03688d8fd
jaber-1988 <jaber1988.1988@gmail.com>
2022-01-13 10:07:38 +0100

    jaber

---
+++ index.js
@@ -0,0 +1,5 @@
+const namen =["Pippi Longstocking", "Bartholomew Roberts","Hans Holbein the Younger","gorg","adam"];
+const kleiner= namen.filter((nam)=>nam.length<=15);
+console.log(kleiner)
+const länger= namen.filter((nam2)=>nam2.length>15);
+console.log(länger)
