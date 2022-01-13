69d7e203713ce350416a74e174e5cffa38ae5917
Mak513 <osei.makangu@hotmail.com>
2022-01-13 10:08:30 +0100

    osei

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const namen = ["pippi","Lonstocking","Bartholomew","Roberts","Hans the Younger"];
+const namen = [
+  "Pippi",
+  "Lonstocking",
+  "Bartholomew",
+  "Roberts",
+  "Hans the Younger",
+];
 
-const array = namen.filter((item) => {
-return item.length <= 15;
-});
+const array = namen.filter((item) => item.length <= 15);
 
-const array2 = namen.filter((item) => {
-  return item.length >= 15;
-});
+const array2 = namen.filter((item) => item.length >= 15);
 console.log(namen);
-console.log("Array",array);
-console.log("Array2",array2);
-
+console.log("Array", array);
+console.log("Array2", array2);
