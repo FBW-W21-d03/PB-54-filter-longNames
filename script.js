51b1e41d44604f7360ef9fd3e1fecc75896d8c51
Vens <v.kolev@alfatrex.com>
2022-01-13 10:04:51 +0100

    vens fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // ## Aufgabenstellung - Teil 1
 // Schreibe ein Array mit mindestens fünf Namen und folgenden Personen: 'Pippi Longstocking', 'Bartholomew Roberts' und 'Hans Holbein the Younger'.
-const namen = ['Pippi Longstocking', 'Bartholomew Roberts', 'Hans Holbein the Younger', 'Bartholomew'];
+const namen = ['Pippi Longstocking', 'Bartholomew Roberts', 'Hans Holbein the Younger', 'Bartholomew', "Aladin"];
 namen.sort();
 // ## Aufgabenstellung - Teil 2
 // Wende nun die filter()-Methode auf das Array an, um nur Namen zu filtern, deren Länge kleiner oder gleich 15 ist.
